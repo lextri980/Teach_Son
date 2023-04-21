4ff116fcd7da4466616fa7394576887f893753a0
lextri980 <lextri980@gmail.com>
2023-04-21 21:27:41 +0700

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-bind:class="count % 2 === 0 ? 'show' : 'hidden'">{{ count }}</p>
+    <input v-if="inputType === 'text'" type="text">
+    <input v-else type="date">
+    <button @click="showInput">{{ inputType === 'text' ? 'Text' : 'Date time' }}</button>
+    <div class="loop">
+      <p v-for="(item, index) in products" :key="index">{{ item.name }}</p>
+    </div>
+    <input type="text" v-model="product" />
+    <button @click="submitProduct">Submit</button>
+    <HelloWorld :products="products" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { HelloWorld },
+  data() {
+    return {
+      product: '',
+      product1Way: '',
+      products: [
+        { id: 1, name: 'milk' },
+        { id: 2, name: 'rice' },
+        { id: 3, name: 'laptop' },
+        { id: 4, name: 'fruit' },
+        { id: 5, name: 'beverage' },
+      ],
+      message: 'Count message',
+      count: 3,
+      isShow: true,
+      inputType: 'text',
+    }
+  },
+  methods: {
+    submitProduct() {
+      console.log(this.product)
+    },
+    showPassword() {
+      this.isShow = !this.isShow
+    },
+    countUp() {
+      return this.count + 2
+    },
+    showInput() {
+      if (this.inputType === 'text') {
+        this.inputType = 'datetime'
+      } else {
+        this.inputType = 'text'
+      }
+    },
+  },
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+}
+
+.show {
+  display: block;
+}
+
+.hidden {
+  display: none
 }
 </style>
---
+++ src/page/dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='dashboard-container'>
+    dashboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard-vue',
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style scoped></style>
