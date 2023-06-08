44a675b737ecd4c1e74cde2b9ae49aa1662fc0e3
lextri980 <lextri980@gmail.com>
2023-06-08 21:28:34 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,110 @@
 <template>
   <div id="app">
-    <!-- <p v-bind:class="count % 2 === 0 ? 'show' : 'hidden'">{{ count }}</p>
-    <input v-if="inputType === 'text'" type="text">
-    <input v-else type="date">
-    <button @click="showInput">{{ inputType === 'text' ? 'Text' : 'Date time' }}</button>
-    <div class="loop">
-      <p v-for="(item, index) in products" :key="index">{{ item.name }}</p>
-    </div>
-    <input type="text" v-model="product" />
-    <button @click="submitProduct">Submit</button>
-    <HelloWorld :products="products" /> -->
-    <ChangeTab/>
+    <ChangeTab />
+    <Table :todoList="todoList" @deleteItem="deleteItem"></Table>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import ChangeTab from './page/changeTab.vue'
+import ChangeTab from "./page/changeTab.vue";
+import Table from "./components/Table.vue";
+
 export default {
-  name: 'App',
-  components: { ChangeTab },
+  name: "App",
+  components: { ChangeTab, Table },
   data() {
     return {
-      product: '',
-      product1Way: '',
+      product: "",
+      product1Way: "",
       products: [
-        { id: 1, name: 'milk' },
-        { id: 2, name: 'rice' },
-        { id: 3, name: 'laptop' },
-        { id: 4, name: 'fruit' },
-        { id: 5, name: 'beverage' },
+        { id: 1, name: "milk" },
+        { id: 2, name: "rice" },
+        { id: 3, name: "laptop" },
+        { id: 4, name: "fruit" },
+        { id: 5, name: "beverage" },
       ],
-      message: 'Count message',
+      message: "Count message",
       count: 3,
       isShow: true,
-      inputType: 'text',
-    }
+      inputType: "text",
+      width: 1,
+      height: 2,
+      some: {
+        nested: {
+          key: 0,
+        },
+      },
+      todoList: [
+        { id: 1, name: "Take an SAD exam", description: "2 days" },
+        { id: 2, name: "Take an MLA exam", description: "2 days" },
+        { id: 3, name: "Take an DSA exam", description: "3 days" },
+        { id: 4, name: "Take an CAL exam", description: "4 days" },
+        { id: 5, name: "Take an NSE exam", description: "4 days" },
+        { id: 6, name: "Take an DBS exam", description: "4 days" },
+        { id: 7, name: "Take an DBA exam", description: "6 days" },
+        { id: 8, name: "Take an BDM exam", description: "7 days" },
+      ],
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {
+    this.calling();
+  },
+  beforeDestroy() {},
+  destroyed() {},
+  computed: {
+    perimeter: {
+      get() {
+        let pOfRectangle;
+        if (!this.width) {
+          pOfRectangle = (1 + this.height) * 2;
+        } else if (!this.height) {
+          pOfRectangle = (1 + this.width) * 2;
+        } else {
+          pOfRectangle = (this.height + this.width) * 2;
+        }
+        return pOfRectangle;
+      },
+    },
+  },
+  watch: {
+    "some.nested.key"() {
+      this.calling();
+    },
   },
   methods: {
-    submitProduct() {
-      console.log(this.product)
+    deleteItem(id) {
+      this.todoList = this.todoList.filter((item) => {
+        return item.id !== id;
+      });
+    },
+    upNumber() {
+      this.some.nested.key += 1;
+    },
+
+    calling() {
+      // console.log("change recalled " + this.some.nested.key);
+    },
+    called() {
+      return 3;
     },
     showPassword() {
-      this.isShow = !this.isShow
+      this.isShow = !this.isShow;
     },
     countUp() {
-      return this.count + 2
+      return this.count + 2;
     },
     showInput() {
-      if (this.inputType === 'text') {
-        this.inputType = 'datetime'
+      if (this.inputType === "text") {
+        this.inputType = "datetime";
       } else {
-        this.inputType = 'text'
+        this.inputType = "text";
       }
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -71,6 +120,6 @@ export default {
 }
 
 .hidden {
-  display: none
+  display: none;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="table-container">
+    <!-- <b-table striped hover :items="todoList"></b-table> -->
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in todoList" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.description }}</td>
+          <td>
+            <b-button variant="danger" @click="$emit('deleteItem', item.id)"
+              >Delete</b-button
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { BButton } from "bootstrap-vue";
+
+export default {
+  name: "Table-container",
+  props: {
+    todoList: {
+      type: Array,
+    },
+  },
+  components: {
+    // BTable,
+    BButton,
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log(this.todoList);
+  },
+  methods: {},
+};
+</script>
+
+<style lang='scss' scoped>
+table,
+td,
+tr,
+th {
+  border: 1px solid black;
+}
+
+td,
+th {
+  padding: 5px 15px;
+}
+</style>
